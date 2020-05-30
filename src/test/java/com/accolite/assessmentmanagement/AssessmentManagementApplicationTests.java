901effff2d5f802e9e40fb627e8cbba75f02ccc5
nilshah98 <neel12@somaiya.edu>
2020-05-31 03:31:03 +0530

    Spring integrated with Angular

---
+++ src/test/java/com/accolite/assessmentmanagement/AssessmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accolite.assessmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
