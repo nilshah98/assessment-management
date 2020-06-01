76102a0b74f5e6fe9e8603f0663ae8b6721da15d
nilshah98 <neel12@somaiya.edu>
2020-06-01 12:34:14 +0530

    [CHORE] (Config > Security) CSRF Token, Expose /test route for Postman testing

--- src/main/java/com/accolite/assessmentmanagement/config/Security.java
+++ src/main/java/com/accolite/assessmentmanagement/config/Security.java
@@ -1,11 +1,16 @@
 package com.accolite.assessmentmanagement.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
@@ -15,14 +20,22 @@ public class Security extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http.cors().and()
+                .csrf()
+                .disable()
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .authorizeRequests(a -> a
-                        .antMatchers("/", "/error", "/webjars/**","/oauth2/**","/*.css","/*.js*","/assets/**").permitAll()
+                        .antMatchers("/", "/error", "/webjars/**","/oauth2/**","/*.css","/*.js*","/assets/**","/test/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .oauth2Login();
+                .logout()
+                .permitAll()
+                .logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+                }).and()
+                .oauth2Login().defaultSuccessUrl("/");
         // @formatter:on
     }
 }
