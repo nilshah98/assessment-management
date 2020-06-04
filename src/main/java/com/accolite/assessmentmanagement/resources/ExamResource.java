9e17f4c908e7c78c378429a747aef0d885d57bc6
nilshah98 <neel12@somaiya.edu>
2020-06-04 20:14:00 +0530

    [REFACTOR] (ExamResource) ExamResource => ExamResource + ExamService

--- src/main/java/com/accolite/assessmentmanagement/resources/ExamResource.java
+++ src/main/java/com/accolite/assessmentmanagement/resources/ExamResource.java
@@ -5,6 +5,7 @@ import com.accolite.assessmentmanagement.repository.QuestionRepository;
 import com.accolite.assessmentmanagement.repository.QuizRepository;
 import com.accolite.assessmentmanagement.repository.ResultRepository;
 import com.accolite.assessmentmanagement.repository.UserRepository;
+import com.accolite.assessmentmanagement.services.ExamService;
 import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -18,108 +19,35 @@ import java.util.stream.StreamSupport;
 public class ExamResource {
 
 //     Repositories
-    private final ResultRepository resultRepository;
-    private final QuizRepository quizRepository;
-    private final QuestionRepository questionRepository;
-    private final UserRepository userRepository;
+    private final ExamService examService;
 
 //    Constructor
-    public ExamResource(ResultRepository resultRepository, QuizRepository quizRepository,
-                        QuestionRepository questionRepository, UserRepository userRepository){
-        this.resultRepository = resultRepository;
-        this.quizRepository = quizRepository;
-        this.questionRepository = questionRepository;
-        this.userRepository = userRepository;
+    public ExamResource(ExamService examService){
+        this.examService = examService;
     }
 
     @GetMapping("/api/exams")
     public List<Quiz> getAllExams(){
-        List<Quiz> quizes = StreamSupport.stream(quizRepository.findAll().spliterator(), false)
-                .collect(Collectors.toList());
-
-//        Make each correct option -1
-        for(Quiz q: quizes){
-            for(Question que: q.getQuestions()){
-                que.setCorrectOption(null);
-            }
-        }
-        return quizes;
+        return  examService.getAllExams();
     }
 
     @GetMapping("/api/exam/result/{quizId}")
     public Result getResult(@PathVariable Long quizId, @AuthenticationPrincipal OAuth2User principal){
         String userId = (String) principal.getAttribute("sub");
-//        Initially no results found
-        Result res = new Result(quizId, -1L, -1L);
-
-        for(Result r : resultRepository.findAll()){
-            if(r.getQuizId().equals(quizId) && r.getUser().getId().equals(userId)){
-                res = r;
-                break;
-            }
-        }
-        return res;
+        return examService.getResultByQuiIdAndUserId(quizId, userId);
     }
 
     @GetMapping("/api/exam/{quizId}")
     public Quiz getExam(@PathVariable Long quizId, @AuthenticationPrincipal OAuth2User principal){
         String userId = (String) principal.getAttribute("sub");
-        boolean userHasTakenTest = false;
-        Quiz exam = new Quiz();
-
-//        Check in result, whether userId with the said quizId
-        for(Result r : resultRepository.findAll()) {
-            if(r.getQuizId().equals(quizId) && r.getUser().getId().equals(userId)){
-                exam = new Quiz("Test already taken", "Can take test only once, check results");
-                exam.setId(-1L);
-                userHasTakenTest = true;
-                break;
-            }
-        }
-
-//        Check if user has not taken exam, generate exam, ie. remove correct option
-        if(!userHasTakenTest) {
-            exam = quizRepository.findById(quizId).get();
-//            Loop through questions and allot correctOption as -1
-            for(Question q : exam.getQuestions()){ q.setCorrectOption(null); }
-        }
-
-        return exam;
+        return examService.getExamByQuizIdAndUserId(quizId, userId);
     }
 
 
     @PostMapping("/api/exam/{quizId}")
     public Result evalExam(@RequestBody Quiz exam, @PathVariable Long quizId, @AuthenticationPrincipal OAuth2User principal){
-//        Extract userId
         String userId = (String) principal.getAttribute("sub");
-
-//        Extract questions
-        Set<Question> questions = exam.getQuestions();
-//        Get total number of questions
-        Long total = (long) questions.size();
-//        Now check each answer
-
-        Long score = 0L;
-        for (Question q : questions){
-//            Get correctOption for question
-            Long qid = q.getId();
-            Question currQ = questionRepository.findById(qid).get();
-
-//            Note that user input stored in correctOption
-//            Take care response is 0 indexed, so need to subtract 1 while checking
-            if(currQ.getCorrectOption()-1 == q.getCorrectOption()) score += 1;
-        }
-
-        Result result = new Result(quizId, score, total);
-
-//        Get user and add
-        User curr = userRepository.findById(userId).get();
-
-//        Set user in results
-        result.setUser(curr);
-
-//        Save and return r
-        return resultRepository.save(result);
+        return examService.evaluateExam(exam, quizId, userId);
 
     }
 }
---
+++ src/main/java/com/accolite/assessmentmanagement/services/ExamService.java
@@ -0,0 +1,135 @@
+package com.accolite.assessmentmanagement.services;
+
+import com.accolite.assessmentmanagement.models.Question;
+import com.accolite.assessmentmanagement.models.Quiz;
+import com.accolite.assessmentmanagement.models.Result;
+import com.accolite.assessmentmanagement.models.User;
+import com.accolite.assessmentmanagement.repository.QuestionRepository;
+import com.accolite.assessmentmanagement.repository.QuizRepository;
+import com.accolite.assessmentmanagement.repository.ResultRepository;
+import com.accolite.assessmentmanagement.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class ExamService {
+
+    //     Repositories
+    private final ResultRepository resultRepository;
+    private final QuizService quizService;
+    private final QuestionService questionService;
+    private final UserService userService;
+
+    //    Constructor
+    public ExamService(ResultRepository resultRepository, QuizService quizService,
+                        QuestionService questionService, UserService userService){
+        this.resultRepository = resultRepository;
+        this.quizService = quizService;
+        this.questionService = questionService;
+        this.userService = userService;
+    }
+
+    public List<Quiz> getAllExams(){
+        List<Quiz> quizes = quizService.getAllQuizes();
+
+//        Make each correct option -1, so as to not view in Network tabs
+        for(Quiz q: quizes){
+            for(Question que: q.getQuestions()){
+                que.setCorrectOption(null);
+            }
+        }
+        return quizes;
+    }
+
+
+
+    @Transactional
+    public List<Result> getAllResults() {
+        return StreamSupport.stream(resultRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+    }
+
+
+    @Transactional
+    public Result saveResult(Result result){
+        return resultRepository.save(result);
+    }
+
+
+
+    public Result getResultByQuiIdAndUserId(Long quizId, String userId){
+        Result result = new Result(quizId, -1L, -1L);
+        for(Result r : getAllResults()){
+            if(r.getQuizId().equals(quizId) && r.getUser().getId().equals(userId)){
+                result = r;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+
+    public Quiz getExamByQuizIdAndUserId(Long quizId, String userId){
+//        If user has already taken exam, return empty quiz
+        boolean userHasTakenTest = true;
+        Quiz exam = new Quiz();
+
+        Result res = getResultByQuiIdAndUserId(quizId, userId);
+        if(res.getScore().equals(-1L)) userHasTakenTest = false;
+
+        if(userHasTakenTest) {
+            exam = new Quiz("Test already taken", "Can only take test once");
+            exam.setId(-1L);
+        }
+        else{
+            exam = quizService.getQuizById(quizId);
+            for(Question q : exam.getQuestions()){ q.setCorrectOption(null); }
+        }
+
+        return exam;
+    }
+
+
+
+    public Result evaluateExam(Quiz exam, Long quizId, String userId){
+
+//        Extract questions
+        Set<Question> questions = exam.getQuestions();
+//        Get total number of questions
+        Long total = (long) questions.size();
+//        Now check each answer
+
+        Long score = 0L;
+        for (Question q : questions){
+//            Get correctOption for question
+            Long qid = q.getId();
+            Question currQ = questionService.getQuestionById(quizId);
+
+//            Note that user input stored in correctOption
+//            Take care response is 0 indexed, so need to subtract 1 while checking
+            if(currQ.getCorrectOption()-1 == q.getCorrectOption()) score += 1;
+        }
+
+        Result result = new Result(quizId, score, total);
+
+//        Get user and add
+        User curr = userService.getUserById(userId);
+
+//        Set user in results
+        result.setUser(curr);
+
+//        Save and return r
+        return saveResult(result);
+
+    }
+
+
+
+
+}
